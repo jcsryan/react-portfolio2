ec5e2f555d3188c6761b4115c36fe853ea0761f4
JCSR <johnconnorsryan@gmail.com>
2020-08-07 22:38:59 -0400

    email container

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react'
 function Contact() {
     return(
         <div class="contact">
-            <div class="contact-info">
+            <div className="contact-info">
                 <p id="emailcontainer">contact me:<br></br> <a id="emailid" href="mailto: johnconnorsryan@gmail.com">Email </a></p>
                 
             </div>
