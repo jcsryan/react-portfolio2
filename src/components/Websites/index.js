17aeaea5a39f7e2409a385fbb54b723def14b659
JCSR <johnconnorsryan@gmail.com>
2020-09-04 15:39:00 -0400

    css changes to link in webpage and contact

--- src/components/Websites/index.js
+++ src/components/Websites/index.js
@@ -11,30 +11,52 @@ function Websites() {
         <div class="row">
             <div class="column">
                 <img src={Logo0} alt="webpage screenshot"></img>
-                <a  class="big-button" id="link" href="https://jcsryan.github.io/run-buddy/" target="_blank" rel="noopener noreferrer">Visit</a>
+                <div class="center-btn">
+                <a  class="bt more-bt" id="link" href="https://jcsryan.github.io/run-buddy/" target="_blank" rel="noopener noreferrer">
+                <span class="fl"></span><span class="sfl"></span><span class="cross"></span><i></i>
+                <p>Visit</p>
+                </a>
+                </div>
             </div>
 
             <div class="column">
                 <img src={Logo1} alt="webpage screenshot"></img>
-                <a  class="big-button" id="link" href="https://stormy-scrubland-17368.herokuapp.com/" target="_blank" rel="noopener noreferrer">Visit</a>
-
+                <div class="center-btn">
+                <a  class="bt more-bt" id="link" href="https://stormy-scrubland-17368.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                <span class="fl"></span><span class="sfl"></span><span class="cross"></span><i></i>
+                <p>Visit</p>
+                </a>
+                </div>
             </div>
 
             <div class="column">
                 <img src={Logo2} alt="webpage screenshot"></img>
-                <a  class="big-button" id="link" href="https://jcsryan.github.io/weather-project/" target="_blank" rel="noopener noreferrer">Visit</a>
-
+                <div class="center-btn">
+                <a  class="bt more-bt" id="link" href="https://jcsryan.github.io/weather-project/" target="_blank" rel="noopener noreferrer">
+                <span class="fl"></span><span class="sfl"></span><span class="cross"></span><i></i>
+                <p>Visit</p>
+                </a>
+                </div>
             </div>
 
             <div class="column">
                 <img src={Logo3} alt="webpage screenshot"></img>
-                <a  class="big-button" id="link" href="https://jcsryan.github.io/movie-ranker/" target="_blank" rel="noopener noreferrer">Visit</a>
-
+                <div class="center-btn">
+                <a  class="bt more-bt" id="link" href="https://jcsryan.github.io/movie-ranker/" target="_blank" rel="noopener noreferrer">
+                <span class="fl"></span><span class="sfl"></span><span class="cross"></span><i></i>
+                <p>Visit</p>
+                </a>
+                </div>
             </div>
+
             <div class="column">
                 <img src={Logo4} alt="webpage screenshot"></img>
-                <a  class="big-button" id="link" href="https://stackstudy.herokuapp.com/#Your%20Cards/" target="_blank" rel="noopener noreferrer">Visit</a>
-
+                <div class="center-btn">
+                <a  class="bt more-bt" id="link" href="https://stackstudy.herokuapp.com/#Your%20Cards/" target="_blank" rel="noopener noreferrer">
+                <span class="fl"></span><span class="sfl"></span><span class="cross"></span><i></i>
+                <p>Visit</p>
+                </a>
+                </div>
             </div>
             <div>
                 <br></br>
